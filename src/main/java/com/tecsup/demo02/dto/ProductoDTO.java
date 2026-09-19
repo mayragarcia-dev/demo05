f8e84c008139f9a80adbd2f191dd2d1c34e579a0
mayragarcia-dev <mayjulisarojas@gamil.com>
2026-09-18 22:11:36 -0500

    Agregar DTO de producto

---
+++ src/main/java/com/tecsup/demo02/dto/ProductoDTO.java
@@ -0,0 +1,50 @@
+package com.tecsup.demo02.dto;
+
+import jakarta.validation.constraints.*;
+
+public class ProductoDTO {
+
+    @NotBlank(message = "El nombre es obligatorio")
+    private String nombre;
+
+    @Positive(message = "El precio debe ser mayor a 0")
+    private double precio;
+
+    @Min(value = 0, message = "El stock no puede ser negativo")
+    private int stock;
+
+    @NotBlank(message = "La categoria es obligatoria")
+    private String categoria;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
