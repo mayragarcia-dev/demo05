82ce5125cbbbd1ba74e29f7f9015e8f04513f0ea
mayragarcia-dev <mayjulisarojas@gamil.com>
2026-09-18 22:12:25 -0500

    Agregar busqueda de productos por nombre

--- src/main/java/com/tecsup/demo02/repository/ProductoRepository.java
+++ src/main/java/com/tecsup/demo02/repository/ProductoRepository.java
@@ -2,6 +2,12 @@ package com.tecsup.demo02.repository;
 
 import com.tecsup.demo02.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
+
+    List<Producto> findByNombreContaining(String nombre);
 }
